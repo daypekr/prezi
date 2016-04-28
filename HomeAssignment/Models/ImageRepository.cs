ea6a18096329079b2c85a395d87b2da6bd20e29f
YongUn Choi <daypekr@gmail.com>
2016-04-28 20:32:57 +0900

    Add repository class which is in charge of manage actual data source

---
+++ HomeAssignment/Models/ImageRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using Newtonsoft.Json;
+
+namespace HomeAssignment.Models
+{
+    public class ImageRepository
+    {
+        public List<Image> FetchAllImage()
+        {
+            var dataPath = HostingEnvironment.MapPath(@"~/App_Data/product.json");
+
+            var dataText = File.ReadAllText(dataPath);
+
+            var images = JsonConvert.DeserializeObject<List<Image>>(dataText);
+
+            return images;
+        } 
+    }
+}
