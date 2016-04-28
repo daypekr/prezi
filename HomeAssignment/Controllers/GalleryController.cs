805332d1dc2cf5c25fdc5f13a2dbf5dbe92a70a9
YongUn Choi <daypekr@gmail.com>
2016-04-28 20:22:17 +0900

    add main controller of gallery api

---
+++ HomeAssignment/Controllers/GalleryController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HomeAssignment.Controllers
+{
+    public class GalleryController : ApiController
+    {
+        // GET api/gallery
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/gallery/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+       
+    }
+}
