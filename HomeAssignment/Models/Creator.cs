b175991b6348d7698dbbb7d7f68679d5fd970f0a
YongUn Choi <daypekr@gmail.com>
2016-04-28 20:09:11 +0900

    Add model classes

---
+++ HomeAssignment/Models/Creator.cs
@@ -0,0 +1,13 @@
+﻿namespace HomeAssignment.Models
+{
+    public class Creator
+    {
+        //"creator": {
+        //   "name": "cupidatat excepteur",
+        //   "profileUrl": "http://randomprofile.prezi.com/"
+        //   },
+
+        public string Name { get; set; }
+        public string ProfileUrl { get; set; }
+    }
+}
---
+++ HomeAssignment/Models/Image.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HomeAssignment.Models
+{
+    // Model class for holding information of each item in the sample data
+    
+    public class Image
+    {
+        //"id": "56f137f4d62116d1231786ca",
+        //"title": "Lorem commodo excepteur minim",
+        //"thumbnail": "https://placeimg.com/400/400/any",
+        //"creator": {
+        //   "name": "cupidatat excepteur",
+        //   "profileUrl": "http://randomprofile.prezi.com/"
+        //   },
+        //"createdAt": "July 31, 2015"
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Thumbnail { get; set; }
+        public Creator Creator { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
